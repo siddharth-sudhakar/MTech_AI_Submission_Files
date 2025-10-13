ecf293d336d36c235e7b0ad11289fc652ceaa54b
Siddharth Sudhakar <60390620+siddharth-sudhakar@users.noreply.github.com>
2025-10-13 17:23:18 +0530

    Update CheckPrime.c

--- DSAL/Lab1/CheckPrime.c
+++ DSAL/Lab1/CheckPrime.c
@@ -4,14 +4,26 @@
 
 int main(){
     int n = 0;
+    int isPrime = 1;
+
     printf("Enter a number to check whether prime: ");
     scanf("%d", &n);
 
-    for(int i=2; i*i<=n; i++){
-        if(n%i==0){
-            printf("Not a prime number\n");
-        } else {
-            printf("It is a prime number\n");
+    if (n <= 1) {
+        isPrime = 0;
+    } else {
+        for (int i = 2; i * i <= n; i++){
+            if (n % i == 0){
+                isPrime = 0;
+                break;
+            }
         }
     }
+
+    if (isPrime)
+        printf("It is a prime number\n");
+    else
+        printf("Not a prime number\n");
+
+    return 0;
 }
